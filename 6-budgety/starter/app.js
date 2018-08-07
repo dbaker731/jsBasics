c5eb0d98495755fcdeb5dfd7eb8641a60bb005a0
Daniel Baker <dbaker@apptricitys-iMac.local>
2018-08-07 16:38:47 -0500

    done for day

--- 6-budgety/starter/app.js
+++ 6-budgety/starter/app.js
@@ -1,7 +1,57 @@
 var budgetController = ( function(){
 
-  
+    var Expense = function( id, description, value ) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
 
+    var Income = function( id, description, value ) {
+        this.id = id;
+        this.description = description;
+        this.value = value;
+    };
+
+
+    var data = {
+        allItems: {
+            expense: [],
+            income: []
+        },
+        totals: {
+            exp: 0,
+            inc: 0
+        },
+    };
+
+    return {
+        addItem: function( type, des, val ) {
+            var newItem, ID;
+
+            // Create new ID
+            if( data.allItems[type].length > 0 ){
+                ID = data.allItems[type][ data.allItems[type].length - 1 ].id + 1;
+            } else {
+                ID = 0;
+            }
+            
+
+            // Create new item based on 'inc' or 'exp'
+            if ( type === 'expense' ) {
+                newItem = new Expense( ID, des, val ); 
+            } else if ( type === 'income' ) {
+                newItem = new Income( ID, des, val ); 
+            }
+            
+            // Push to data
+            data.allItems[type].push( newItem );
+
+            console.log( data );
+            
+            // Return new element
+            return newItem;
+        },
+    };
 })();
 
 
@@ -54,9 +104,15 @@ var controller = ( function( budgetCtrl, UICtrl ){
 
     function ctrlAddItem() {
         // TODO: add item to budget controller, add item to the UI, calc budget, display the budget
-        var input = UICtrl.getInput();
+        var input, newItem;
+        // Get field input data
+        input = UICtrl.getInput();
         console.log( input );
-    
+        
+        // Add item to budget controller
+        newItem = budgetCtrl.addItem( input.type, input.description, input.value );
+        console.log(newItem);
+        
     }
    
     return {
