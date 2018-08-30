413eaaa29323bfb61ce431fffd859d8254a93048
Daniel Baker <dbaker@apptricitys-iMac.local>
2018-08-30 17:11:20 -0500

    webpack

--- 9-forkify/webpack.config.js
+++ 9-forkify/webpack.config.js
@@ -1,8 +1,8 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPlugin = require( 'html-webpack-plugin' );
 
 module.exports = {
-    entry: ['babel-polyfill', './src/js/index.js'],
+    entry: [ 'babel-polyfill', './src/js/index.js' ],
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'js/bundle.js'
@@ -10,21 +10,21 @@ module.exports = {
     devServer: {
         contentBase: './dist'
     },
-    plugins: [
+    plugins: [ 
         new HtmlWebpackPlugin({
             filename: 'index.html',
             template: './src/index.html'
         })
-    ],
+     ],
     module: {
         rules: [
             {
                 test: /\.js$/,
-                exclude: /node_modules/,
+                exclude: path.resolve(__dirname, 'node_modules'),
                 use: {
                     loader: 'babel-loader'
                 }
             }
         ]
-    }
+    } 
 };
