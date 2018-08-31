4927dbd1bb55a36a36cc5a2c86414cacc2ef7a64
Daniel Baker <dbaker@apptricitys-iMac.local>
2018-08-31 09:45:58 -0500

    test

--- 9-forkify/src/js/index.js
+++ 9-forkify/src/js/index.js
@@ -1,5 +1,5 @@
 import str from './models/Search';
 import { add, multiply, ID } from './views/searchView';
 
-console.log( `Using imported functions! ${ add( ID, 2 ) } and ${ multiply( 3, 5 ) }. ${ str } ` );
+console.log( `Using imported functions! ${ add( ID, 2 ) } and ${ multiply( 3, 5 ) }. ${ str }` );
 
